789e1722f89b599381c51bf4f5f4a7f0ffe61122
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-10-19 10:55:20 -0400

    Update challenge.cpp

--- lab1/challenge.cpp
+++ lab1/challenge.cpp
@@ -1,5 +1,5 @@
 /**
- * 8 LEDs Challenge - Written by Aritro Saha 2021.10.18
+ * 8 LEDs Challenge - Written by Aritro Saha 2021.10.19
  * This code is written to make 8 LEDs light on and off one
  * after another in a continuous loop using for loops and variables.
  * variables, arrays, for loop. In addition, the delay time will vary 
@@ -15,6 +15,9 @@ int delayTime = 500; // delay time variable, in ms
 // initially set to false since it will be toggled on when the loop() func runs for the first time
 bool speedUpTime = false;
 
+// The order of the LED pins that will be turned on / off
+int ledPins[] = {1, 5, 2, 6, 3, 7, 4, 8};
+
 void setup()
 {
   // set all LED pins as outputs
@@ -27,26 +30,36 @@ void setup()
 void loop()
 {
   // turn LEDs on in the sequence of 1, 5, 2, 6, 3, 7, 4, 8
-  for (int i = 1; i <= 8; i++)
+  for (int i = 0; i <= 7; i++)
   {
-    // if i is odd, turn on the i th LED, otherwise, turn on the i + 3 LED
-    digitalWrite(i % 2 ? i : (i + 3), HIGH);
-    delay(500); // 500 ms delay
+    digitalWrite(ledPins[i], HIGH);
+    delay(delayTime);
   }
 
   // turn LEDs off in the reverse sequence
-  for (int i = 8; i >= 1; i--)
+  for (int i = 7; i >= 0; i--)
   {
-    // if i is odd, turn off the i th LED, otherwise, turn off the i + 3 LED
-    digitalWrite(i % 2 ? i : (i + 3), LOW);
-    delay(500);
+    digitalWrite(ledPins[i], LOW);
+    delay(delayTime); 
   }
 
-  // If it has reached the limits of either fastest or slowest, start doing the opposite (faster -> slower, slower -> faster)
-  if (delayTime == 500 || delayTime == 100) {
-    speedUpTime = !speedUpTime;
+  // TODO: Fix code
+  // If it has reached the limits of either fastest or slowest,
+  // start doing the opposite (faster -> slower, slower -> faster)
+  if (delayTime == 500 || delayTime == 100)
+  {
+    speedUpTime = true;
   }
 
-  // Add to the delay time according to the speedUpTime, if true, it will go faster, otherwise, it will go slower
-  delayTime += speedUpTime ? -100 : 100; 
+  if (delayTime == 100) {
+    speedUpTime = false;
+  }
+
+  // Add to the delay time according to the speedUpTime,
+  // if true, it will go faster, otherwise, it will go slower
+  if (speedUpTime) {
+    delayTime -= 100;
+  } else {
+    delayTime += 100;
+  }
 }
