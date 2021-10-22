4cc2d49e58e527af67264c79b3be87617fe1b763
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-10-22 10:01:55 -0400

    Update challenge.cpp

--- lab2/challenge.cpp
+++ lab2/challenge.cpp
@@ -1,16 +1,21 @@
 /* 
- * US Sensor - Written by Aritro Saha & Tony Lin 2021.10.22
+ * Ultrasonic Sensor - Written by Aritro Saha & Tony Lin 2021.10.22
  * This code is written to sense distance and sound a buzzer using
  * "tone" commands. It also plays Hot Cross Buns when an object is <50cm away
  * from the ultrasonic (US) sensor. 
 */
 
+#include <Arduino.h>
+
 // declare variables for pins
-int GREEN_LED_PIN = 4;
-int RED_LED_PIN = 5;
-int BUZZER_PIN = 6;
-int TRIGGER_PIN = 3;
-int ECHO_PIN = 2;
+int GREEN_LED_PIN = 7;
+int RED_LED_PIN = 4;
+int BUZZER_PIN = 3;
+int TRIGGER_PIN = 12;
+int ECHO_PIN = 13;
+
+// Due to distribution of pins, all of them are put in one place to make it easier to init
+int outputPinsUsed[] = {BUZZER_PIN, RED_LED_PIN, GREEN_LED_PIN, TRIGGER_PIN, ECHO_PIN};
 
 // declare variables for measuring distance using us sensor
 float duration;
@@ -18,13 +23,13 @@ float distance;
 
 void setup()
 {
-  // set pins 3 to 6 outputs
-  for (int i = 3; i <= 6; i++)
+  // init all output pins
+  for (int i = 0; i < 4; i++)
   {
-    pinMode(i, OUTPUT);
+    pinMode(outputPinsUsed[i], OUTPUT);
   }
 
-  pinMode(2, INPUT); // set pin 2 as input
+  pinMode(ECHO_PIN, INPUT); // set echo pin as input
 }
 
 void loop()
@@ -38,11 +43,11 @@ void loop()
   duration = pulseIn(ECHO_PIN, HIGH);
   distance = duration * 0.034 / 2;
   /************** End US Measurement Section ***********/
-  
-  // Toggle LED + Play recognizable tone
+
+  // if distance is less than or equal to 50cm
   if (distance <= 50)
   {
-    // Toggle red LED
+    // toggle red LED
     digitalWrite(RED_LED_PIN, HIGH);
     digitalWrite(GREEN_LED_PIN, LOW);
 
@@ -51,12 +56,12 @@ void loop()
     for (int i = 0; i < 2; i++)
     {
       // each delay is 250ms so it matches 4/4 time
-      tone(BUZZER_PIN, 123.47);
+      tone(BUZZER_PIN, 493.88);
       delay(250);
-      tone(BUZZER_PIN, 110.00);
+      tone(BUZZER_PIN, 220.00);
       delay(250);
       tone(BUZZER_PIN, 98.00);
-      delay(500); 
+      delay(500);
     }
 
     // Middle section
@@ -65,9 +70,9 @@ void loop()
     {
       // G on the piano
       tone(BUZZER_PIN, 98.00);
-      // Delay is 100ms to recreate the effect of stopping for a second to breath
+      // Delay is 100ms to recreate the effect of stopping for a second to tongue
       delay(100);
-      
+
       // Turn it off to recreate the effect of stop playing
       noTone(BUZZER_PIN);
       delay(25);
@@ -77,7 +82,7 @@ void loop()
     for (int i = 0; i <= 4; i++)
     {
       // A on the piano
-      tone(BUZZER_PIN, 110.00);
+      tone(BUZZER_PIN, 220.00);
       delay(100);
 
       noTone(BUZZER_PIN);
@@ -85,35 +90,39 @@ void loop()
     }
 
     // Last 3 notes
-    tone(BUZZER_PIN, 123.47);
+    tone(BUZZER_PIN, 493.88);
     delay(250);
-    tone(BUZZER_PIN, 110.00);
+    tone(BUZZER_PIN, 220.00);
     delay(250);
     tone(BUZZER_PIN, 98.00);
     delay(500);
     /************ End playing of Hot Cross Buns using the buzzer ************/
   }
 
-  // Play a higher frequency on the buzzer as the distance lowers 
+  // Play a higher frequency on the buzzer as the distance lowers
   else if (distance < 60)
   {
-    tone(BUZZER_PIN, 130.81);
+    // A5 note
+    tone(BUZZER_PIN, 880.00);
   }
   else if (distance < 70)
   {
-    tone(BUZZER_PIN, 123.47);
+    // B note
+    tone(BUZZER_PIN, 493.88);
   }
   else if (distance < 80)
   {
-    tone(BUZZER_PIN, 110.00);
+    // A note
+    tone(BUZZER_PIN, 220.00);
   }
   else if (distance < 90)
   {
-    tone(BUZZER_PIN, 98.00); 
+    // G note
+    tone(BUZZER_PIN, 98.00);
   }
 
-  // Distance is further than 90cm
-  else 
+  // Distance is further than 90cm, no tone
+  else
   {
     noTone(BUZZER_PIN);
   }
@@ -124,6 +133,6 @@ void loop()
     digitalWrite(GREEN_LED_PIN, HIGH);
     digitalWrite(RED_LED_PIN, LOW);
   }
-  
+
   delay(10); // small delay to save system resources
 }
